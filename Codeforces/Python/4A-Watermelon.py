1ba7a1bf09edc9feb2a52d9c4201b3941bf28916
Sebastian Angulo <sebasanguloc@gmail.com>
2024-03-03 23:34:21 -0500

    Algorithm-Codeforces Watermelon

---
+++ Codeforces/Python/4A-Watermelon.py
@@ -0,0 +1,40 @@
+# SOLUTION
+
+w = int(input())
+ 
+if w % 2 == 0 and w != 2:
+    print('YES')
+else:
+    print('NO')
+
+'''
+TEST #1
+input:
+8
+output:
+YES
+
+TEST #2
+input:
+5
+output:
+NO
+
+TEST #3
+input:
+4
+output:
+YES
+
+TEST #4
+input:
+3
+output:
+NO
+
+TEST #5
+input:
+2
+output:
+NO
+'''
